7cba3b0b9ab2ff065df156c730cac399169342ef
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2024-08-04 22:15:19 +0300

    added folder after using cmake command check readme paragraph(CMAKE)

---
+++ sqlite_build/CMakeFiles/sqlite.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/ivanzhuravlev/Desktop/tz/infotecs_Task/sqlite/shell.c" "CMakeFiles/sqlite.dir/shell.c.o" "gcc" "CMakeFiles/sqlite.dir/shell.c.o.d"
+  "/Users/ivanzhuravlev/Desktop/tz/infotecs_Task/sqlite/sqlite3.c" "CMakeFiles/sqlite.dir/sqlite3.c.o" "gcc" "CMakeFiles/sqlite.dir/sqlite3.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ sqlite_build/CMakeFiles/sqlite.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sqlite.dir/shell.c.o"
+  "CMakeFiles/sqlite.dir/shell.c.o.d"
+  "CMakeFiles/sqlite.dir/sqlite3.c.o"
+  "CMakeFiles/sqlite.dir/sqlite3.c.o.d"
+  "sqlite"
+  "sqlite.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sqlite.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
